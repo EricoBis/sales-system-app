e1e7ea0c51ffd2042cc892332bd02521f1d11bc9
Érico Bis <ericobis.santos@gmail.com>
2023-11-01 00:05:21 -0300

    update | handleAddCartItem

--- src/app/hooks/context/CartContext.tsx
+++ src/app/hooks/context/CartContext.tsx
@@ -16,7 +16,11 @@ const CartContext = createContext<CartContextType>({
 export const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const [cart, setCart] = useState<Cart>({ itemList: [] });
 
-  const updateItemAmount = ( listToUpdate: CartItem[], idToUpdate: number, oper: number): CartItem[] => {
+  const updateItemAmount = (
+    listToUpdate: CartItem[],
+    idToUpdate: number,
+    oper: number
+  ): CartItem[] => {
     return listToUpdate.map((item) => ({
       ...item,
       amount: item.productId === idToUpdate ? item.amount + oper : item.amount,
@@ -26,7 +30,9 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const handleRemoveCartItem = useCallback((productId: number) => {
     setCart((prevCart) => ({
       ...prevCart,
-      itemList: prevCart.itemList.filter((item) => item.productId !== productId),
+      itemList: prevCart.itemList.filter(
+        (item) => item.productId !== productId
+      ),
     }));
   }, []);
 
@@ -35,21 +41,12 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
       const itemAlreadyOnCart = prevCart.itemList.find(
         (item) => item.productId === newItem.productId
       );
-  
+
       if (itemAlreadyOnCart) {
         // If the item already exists in the cart, create an updated copy of the cart
         return {
           ...prevCart,
-          itemList: prevCart.itemList.map((item) => {
-            if (item.productId === newItem.productId) {
-              // Update the quantity only for the corresponding item
-              return {
-                ...item,
-                amount: item.amount + newItem.amount,
-              };
-            }
-            return item;
-          }),
+          itemList: updateItemAmount(prevCart.itemList, newItem.productId, 1),
         };
       } else {
         // If the item does not exist in the cart, add it
