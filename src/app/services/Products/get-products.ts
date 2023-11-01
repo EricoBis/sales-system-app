1d98d25146a3cd265763eb95509335e13d3823de
Érico Bis <ericobis.santos@gmail.com>
2023-11-01 01:05:37 -0300

    update | get-products.ts

--- src/app/services/Products/get-products.ts
+++ src/app/services/Products/get-products.ts
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 export async function getProducts() {
 
-    return axios.get('http://localhost:8080/products')
-        .then(response => {
-            return response?.data || [];
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
+    try {
+        const response = await axios.get('http://localhost:8080/products');
+        return response?.data || [];
+    } catch (error) {
+        console.error(error);
+    }
 }
