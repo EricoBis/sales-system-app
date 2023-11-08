eb52fe10290f4473005fcdd1fe7c10f6912bb1cd
Érico Bis <ericobis.santos@gmail.com>
2023-11-08 10:31:04 -0300

    modified | Cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+'use client'
+
+import React, { useContext } from "react";
+import CartContent from "@/components/Cart/CartContent/CartContent";
+import EmptyCart from "@/components/Cart/CartContent/EmptyCart";
+import { CartContext } from "@/context/CartContext";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  const { cart } = useContext(CartContext);
+
+  return <>{cart.itemList.length === 0 ? <EmptyCart /> : <CartContent />}</>; //TODO - TROCAR !== QUANDO PRONTO
 }
