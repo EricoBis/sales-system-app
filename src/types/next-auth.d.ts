5f9e1d090e42f93e465a2949716954d9596c214b
Érico Bis <ericobis.santos@gmail.com>
2023-11-14 22:50:18 -0300

    add | User interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,9 @@
 import NextAuth from "next-auth";
+import { User } from "./User";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: number;
-      name: string;
-      email: string;
-      token: string;
-    };
+    user: User
   }
 }
+
