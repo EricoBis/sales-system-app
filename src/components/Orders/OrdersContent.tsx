0da3dd82259f487fc42f3e57756cfee3ad1bd431
Érico Bis <ericobis.santos@gmail.com>
2023-11-18 01:58:56 -0300

    add | Empty state to OrdersContent

--- src/components/Orders/OrdersContent.tsx
+++ src/components/Orders/OrdersContent.tsx
@@ -5,14 +5,15 @@ import { getAllBudgets } from "@/services/Budgets/get-all-client-budgets";
 import {
   Accordion,
   AccordionItem,
+  Button,
   Chip,
+  Link,
 } from "@nextui-org/react";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { LuPackage } from "react-icons/lu";
 import { setBudgetDone } from "@/services/Budgets/set-budget-done";
 import { useRouter } from "next/navigation";
-import { Product } from "@/utils/types/Product";
 import AccordionItemContent from "./AccordionItemContent";
 
 interface OrdersContentProps {
@@ -31,16 +32,16 @@ function OrdersContent({ user }: OrdersContentProps) {
   }, []);
 
   const handleFinalizeOrder = async (orderId: number) => {
-    if(user) {
+    if (user) {
       try {
-         await setBudgetDone(orderId, user);
-         router.replace(`/cart/checkout/receipt/${orderId}`);
+        await setBudgetDone(orderId, user);
+        router.replace(`/cart/checkout/receipt/${orderId}`);
       } catch (error) {
         localStorage.setItem("checkout_error", (error as Error).message);
         router.replace(`/cart/checkout/receipt/${orderId}`);
       }
     }
-  }
+  };
 
   const getChipStatus = (isBudgetDone: boolean) => {
     return isBudgetDone ? (
@@ -66,24 +67,39 @@ function OrdersContent({ user }: OrdersContentProps) {
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 items-start justify-center mx-auto max-w-4xl">
-        <div className="flex flex-row align-middle">
-          <LuPackage className="w-8 h-8" />
-          <h1 className="text-xl font-semibold ml-2">Meus Pedidos</h1>
+      {budgetList.length !== 0 ? (
+        <div className="grid grid-cols-1 gap-4 items-start justify-center mx-auto max-w-4xl">
+          <div className="flex flex-row align-middle">
+            <LuPackage className="w-8 h-8" />
+            <h1 className="text-xl font-semibold ml-2">Meus Pedidos</h1>
+          </div>
+          <Accordion variant="shadow">
+            {budgetList &&
+              budgetList.map((budget, index) => (
+                <AccordionItem
+                  key={index}
+                  subtitle={getChipStatus(budget.done)}
+                  title={`Pedido: #${budget.orderId}`}
+                >
+                  <AccordionItemContent
+                    budget={budget}
+                    finalizeOrder={handleFinalizeOrder}
+                  />
+                </AccordionItem>
+              ))}
+          </Accordion>
         </div>
-        <Accordion variant="shadow">
-          {budgetList &&
-            budgetList.map((budget, index) => (
-              <AccordionItem
-                key={index}
-                subtitle={getChipStatus(budget.done)}
-                title={`Pedido: #${budget.orderId}`}
-              >
-                <AccordionItemContent budget={budget} finalizeOrder={handleFinalizeOrder}/>
-              </AccordionItem>
-            ))}
-        </Accordion>
-      </div>
+      ) : (
+        <div className="flex flex-col items-center mt-32 mb-10">
+          <LuPackage className="text-gray-300 w-52 h-52" />
+          <h1 className="font-bold m-5">
+            Você ainda não possui pedidos! Continue explorando
+          </h1>
+          <Button href="/" as={Link} color="primary" variant="shadow">
+            Continuar explorando
+          </Button>
+        </div>
+      )}
     </>
   );
 }
