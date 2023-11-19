9649540d0dc2aec7a40a35a652f72e5e2085d6c1
Érico Bis <ericobis.santos@gmail.com>
2023-11-18 23:37:09 -0300

    modified | Next Auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,8 @@
+import { authConfig } from "@/lib/auth";
 import { API_URL } from "@/services/api-url";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-   
-      credentials: {
-        email: { label: "E-mail", type: "text", placeholder: "@email.com" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-
-        const res = await fetch(`${API_URL}/auth/login`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password,
-          }),
-        });
-        const user = await res.json()
-  
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user
-        }
-        // Return null if user data could not be retrieved
-        return null
-      }
-    })
-  ],
-  
-  callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token, user }) {
-      session.user = token as any;
-      return session;
-    },
-  },
-
-  pages: {
-    signIn: "/login",
-  },
-
-})
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST }
